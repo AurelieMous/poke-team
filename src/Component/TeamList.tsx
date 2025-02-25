537873267acf4aa76413bc103681dc116ebbaf0c
Aurélie <naoorelie@gmail.com>
2025-02-25 17:19:39 +0100

    Fonctionnalité supprimé de l'équipe ok

--- src/Component/TeamList.tsx
+++ src/Component/TeamList.tsx
@@ -1,29 +1,54 @@
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "@/redux/store.ts";
-import {Button, Image} from "@chakra-ui/react";
-
+import {Badge, Button, Card, Flex, Image, Text} from "@chakra-ui/react"
+import {remove} from "@/redux/slices/team.slice.ts";
 
 export default function TeamList() {
 
     const dispatch = useDispatch();
     const team = useSelector((state: RootState) => state.team.pokemonsTeams);
 
+    const handlerRemovePokemon = (pokedex_id: number) => {
+        dispatch(remove(pokedex_id));
+    }
 
     return (
         <>
-            <ul>
+            <Flex wrap="wrap" gap={5} justify="center">
                 {team.map((pokemon) => (
-                    <li key={pokemon.pokedex_id}>
+                    <Card.Root maxW="sm" overflow="hidden" key={pokemon.pokedex_id}>
                         <Image
-                            maxW="250px"
                             src={pokemon.sprites.regular}
                             alt={pokemon.name.fr}
                         />
-                        # {pokemon.pokedex_id} - {pokemon.name.fr}
-                        <Button>Retirer</Button>
-                    </li>
+                        <Card.Body gap="2">
+                            <Card.Title>#{pokemon.pokedex_id} - {pokemon.name.fr}</Card.Title>
+                            <Card.Description>
+                                {pokemon.category}
+                            </Card.Description>
+                            <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                                {pokemon.types?.map((type, index) => (
+                                    <Badge key={index} colorScheme="teal">
+                                        <Image
+                                            objectFit="cover"
+                                            maxW="20px"
+                                            src={type.image}
+                                            alt={type.name}
+                                        />
+                                        {type.name}
+                                    </Badge>
+                                ))}
+                            </Text>
+                        </Card.Body>
+                        <Card.Footer gap="2">
+                            <Button colorPalette={"red"} variant={"surface"} onClick={() => handlerRemovePokemon(pokemon.pokedex_id)}>
+                                Retirer de l'équipe
+                            </Button>
+                        </Card.Footer>
+                    </Card.Root>
                 ))}
-            </ul>
+            </Flex>
+
         </>
     )
 }
