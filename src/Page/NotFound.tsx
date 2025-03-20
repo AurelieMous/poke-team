6be18bdfad22aab0b8f4ab9694e557e0a6b58ced
Aurélie <naoorelie@gmail.com>
2025-03-20 16:58:57 +0100

    modif page erreur

--- src/Page/NotFound.tsx
+++ src/Page/NotFound.tsx
@@ -1,17 +1,26 @@
 import Header from "@/Component/Header.tsx";
-import {Container, Flex, Heading, Link} from "@chakra-ui/react";
+import {Button, Container, Flex, Heading} from "@chakra-ui/react";
 import Footer from "@/Component/Footer.tsx";
+import {useNavigate} from "react-router";
 
 export default function NotFound() {
+    const navigate = useNavigate();
+
+    const handleMenu = () => {
+        navigate("/list");
+    }
     return (
         <>
             <Header />
             <Container maxW="md" height="100vh">
                 <Flex direction="column" justifyContent="center" alignItems="center" height="100%">
-                    <Heading size="3xl" textAlign="center" mb={4}>
-                        Pokémon non trouvé
+                    <Heading size="5xl" textAlign="center" mb={4}>
+                        Erreur 404
                     </Heading>
-                    <Link href="/list" color={"blue"}>Retour à la liste des Pokemons</Link>
+                    <Heading size="3xl" textAlign="center" mb={4}>
+                        Pokémon ou page non trouvé
+                    </Heading>
+                    <Button onClick={handleMenu} colorPalette={"blue"}>Retour à la liste des Pokemons</Button>
                 </Flex>
             </Container>
             <Footer />
